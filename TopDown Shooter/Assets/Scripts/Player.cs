9d083487fa0035dc4196729ec3d5d723318e3689
KhizarFarooqGameDev <khizarfarooq30@gmail.com>
2020-07-22 09:02:03 +0500

    Player Scripts done

--- TopDown Shooter/Assets/Scripts/Player.cs
+++ TopDown Shooter/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,34 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    Rigidbody2D rb;
+    Animator animator;
+
+    Vector2 moveAmount;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = movementInput.normalized * speed;
+
+        if(movementInput != Vector2.zero) {
+            animator.SetBool("running", true);
+        } else {
+            animator.SetBool("running", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.deltaTime);
     }
 }
